89f2b37746146436e8352e1cc5ede28eeaf50ad0
Ntone Mallo <christian.ntone@institutsaintjean.org>
2023-11-25 11:34:31 +0100

    modify background color

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+describe('AppComponent', () => { //describe est une fonction de jasmin qui permet de regrouper des tests dans des unités
+  beforeEach(() => TestBed.configureTestingModule({ //test
     imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
-  it('should create the app', () => {
+  it('should create the app', () => { 
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
